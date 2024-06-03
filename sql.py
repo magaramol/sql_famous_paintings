8da0eb81bb64bde42ad3d605e1ab0d38a7f584a5
magaramol <amoljb007@gmail.com>
2024-06-04 00:42:26 +0530

    sql cnf

--- sql.py
+++ sql.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
+import chardet
 
-# URL-encode the password
+# URL-encode the password   
 password = quote_plus("Root@1234")
 
 conn_string = f"mysql+pymysql://root:{password}@localhost/painting"
@@ -10,15 +11,33 @@ conn_string = f"mysql+pymysql://root:{password}@localhost/painting"
 db = create_engine(conn_string)
 conn = db.connect()
 
-
-
-#files = ['work']
-
 files = ['artist', 'canvas_size', 'image_link', 'museum_hours', 'museum', 'product_size', 'subject']
 
-
-#  files = ['artist', 'canvas_size', 'image_link', 'museum_hours', 'museum', 'product_size', 'subject', 'work']
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+    return result['encoding']
 
 for file in files:
-    df = pd.read_csv(f'data/{file}.csv')
-    df.to_sql(file,con=conn,if_exists='replace',index=False)
+    file_path = f'data/{file}.csv'
+    encoding = detect_encoding(file_path)
+    df = pd.read_csv(file_path, encoding=encoding)
+    df.to_sql(file, con=conn, if_exists='replace', index=False)
+
+
+
+"""
+Update MySQL Configuration
+
+    Open your MySQL configuration file (usually my.cnf or my.ini).
+    Add the following lines under the [mysqld] section:
+
+    ini
+
+[mysqld]
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci
+
+Restart your MySQL server to apply the changes.
+
+"""
